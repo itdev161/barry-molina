8c8c21340111d02c8e36410bc6730b411276027d
Barry Molina <bazzaboy@gmail.com>
2020-11-14 15:42:52 -0600

    Create a Post Model

---
+++ models/Posts.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: 'ObjectId',
+        ref:'User'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+});
+
+const Post = mongoose.Model('post', PostSchema);
+
+export default Post;
