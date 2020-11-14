99dcf04f2f20f773c38939fc93c00d7d0529645e
Barry Molina <bazzaboy@gmail.com>
2020-11-14 15:53:55 -0600

    Create Post Endpoints

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import User from './models/User';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import auth from './middleware/auth';
+import Post from './models/Post';
 
 // Initialize express application
 const app = express();
@@ -119,6 +120,48 @@ app.post('/api/users',
 
                 returnToken(user, res);
             } catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+/**
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+    '/api/posts',
+    [
+        auth,
+        [
+            check('title', 'Title text is required')
+                .not()
+                .isEmpty(),
+            check('body', 'Body text is required')
+                .not()
+                .isEmpty(),
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array() });
+        } else {
+            const { title, body } = req.body;
+            try {
+                const user = await User.findById(req.user.id);
+
+                const post = new Post({
+                    user: user.id,
+                    title: title,
+                    body: body
+                });
+
+                await post.save();
+
+                res.json(post);
+            } catch (error) {
+                console.error(error);
                 res.status(500).send('Server error');
             }
         }
